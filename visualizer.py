63fd758a628fa1e83bf930a150acf16e5269c895
OArikui <Arikui57@outlook.jp>
2026-08-28 13:53:28 +0900

    init: visualizer #4 #12 #8

---
+++ visualizer.py
@@ -0,0 +1,6 @@
+import cv2
+import matplotlib.colors as mcolors
+import matplotlib.patheffects as path_effects
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg
